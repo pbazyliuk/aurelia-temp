7d064a50bf3f1b31b0f7192c01ce7064cc5c6b34
pavlo <notoriousb@bk.ru>
2017-08-09 22:21:54 +0300

    added router config

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
+ 
+  configureRouter(config, router) {
+    this.router = router;
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'home'], title: 'home',       name: 'home',       moduleId: 'home/home', nav: true },
+      { route: 'users',    title: 'users',        name: 'users',      moduleId: 'users/users',   nav: true },
+      { route: 'users/:id', title: 'userDetail',  name: 'userDetail', moduleId: 'userDetail/userDetail'}      
+    ]);
   }
 }
