7197d949ff525aa756d6ae559101d6261742de50
Pavlo <p.bazyliuk@gmail.com>
2017-08-12 20:21:15 +0300

    added custom component

--- src/app.js
+++ src/app.js
@@ -1,35 +1,39 @@
+import { bindable } from "aurelia-framework";
+
 export class App {
-	configureRouter(config, router) {
-		this.router = router;
-		config.title = 'Aurelia';
-		config.map([
-			{
-				route: ['', 'home'],
-				title: 'home',
-				name: 'home',
-				moduleId: 'home/home',
-				nav: true
-			},
-			{
-				route: 'users',
-				title: 'users',
-				name: 'users',
-				moduleId: 'users/users',
-				nav: true
-			},
-			{
-				route: 'form1',
-				title: 'form1',
-				name: 'form1',
-				moduleId: 'form1/form1',
-				nav: true
-			},
-			{
-				route: 'users/:id',
-				title: 'userDetail',
-				name: 'userDetail',
-				moduleId: 'userDetail/userDetail'
-			}
-		]);
-	}
+  isSidebarVisible = 3;
+
+  configureRouter(config, router) {
+    this.router = router;
+    config.title = "Aurelia";
+    config.map([
+      {
+        route: ["", "home"],
+        title: "home",
+        name: "home",
+        moduleId: "home/home",
+        nav: true
+      },
+      {
+        route: "users",
+        title: "users",
+        name: "users",
+        moduleId: "users/users",
+        nav: true
+      },
+      {
+        route: "form1",
+        title: "form1",
+        name: "form1",
+        moduleId: "form1/form1",
+        nav: true
+      },
+      {
+        route: "users/:id",
+        title: "userDetail",
+        name: "userDetail",
+        moduleId: "userDetail/userDetail"
+      }
+    ]);
+  }
 }
